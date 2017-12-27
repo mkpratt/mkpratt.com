24d8a0d2676c770e69af802f36495938395b6942
Michael Pratt <mkpratt6@gmail.com>
2017-12-27 15:05:27 -0700

    updating page content stuff

--- assets/scripts/content-events.js
+++ assets/scripts/content-events.js
@@ -1,8 +1,32 @@
-// var bg = document.querySelectorAll(".bg")[0];
 let bg = document.createElement('div');
 bg.id = 'bg';
 bg.classList.add('bg');
 bg.style.transform = 'skewX(-23deg) translateX(' + getHiddenOffset() + 'px)';
+document.body.appendChild(bg);
+let content = document.createElement('div');
+content.classList.add('content');
+
+let r1 = document.createElement('div');
+let el11 = document.createElement('div');
+el11.classList.add('first-row-el');
+let el12 = document.createElement('div');
+el12.classList.add('first-row-el');
+r1.appendChild(el11);
+r1.appendChild(el12);
+r1.classList.add('row');
+content.appendChild(r1)
+
+let r2 = document.createElement('div');
+let el21 = document.createElement('div');
+el21.classList.add('first-row-el');
+let el22 = document.createElement('div');
+el22.classList.add('first-row-el');
+r2.appendChild(el21);
+r2.appendChild(el22);
+r2.classList.add('row');
+content.appendChild(r2);
+document.body.appendChild(content);
+
 
 bg.addEventListener('transitionend', onTransitionEnd(), false);
 var pos = { x: getHiddenOffset() };
